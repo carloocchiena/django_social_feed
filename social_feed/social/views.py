13722c571e5a89c0ad8641f9e83e4525175eb9b6
Carlo Occhiena <robotfood@virgilio.it>
2022-04-28 23:03:32 +0200

    fix

--- social_feed/social/views.py
+++ social_feed/social/views.py
@@ -17,7 +17,27 @@ class ProfileList(ListView):
 # View of each profile    
 class ProfileDetail(DetailView):
     """View a specific profile"""
-    model = models.Profile  
+    model = models.Profile
+    fields =   ['follows']
+    
+# test per follow-unfollow (che non va)
+# non posso chiamare una funzione post da questo modello
+# però posso chiamare un url dal bottone che attiva una funzione?
+# boh non ne vengo a capo, riproviamo
+def profile(self, request):
+    """Manage user profile, follows and unfollows"""
+    profile = Profile.objects.get(pk=pk)
+    if request.method == 'GET':
+        current_user_profile = request.user.profile
+        data = request.GET
+        action = data.get('follow')
+        if action == 'follow':
+            current_user_profile.follows.add(profile)
+        elif action == 'unfollow':
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
+
+    
     
 # Update user profile
 class ProfileUpdateView(UpdateView):
--- wip.md
+++ wip.md
@@ -1,12 +1,17 @@
-    
-- create html templates
-    - for all posts
+
+
+currently to be done:
+- follow-unfollow logic
+- dashboard
+- registration
+- post creation form
+
+
 
 - check profile_list page and finalize it
 - create login journey (with psw management and such)
 - add functional test
 - check what happens if user upload something different from a picture
-- add follow-unfollow function
 - clean all the dirty code in the files
 
 
