d65efe978c1804b58e180bbb2512f2b38e24a087
Carlo Occhiena <robotfood@virgilio.it>
2022-05-19 13:03:17 +0200

    fix

--- social_feed/social/models.py
+++ social_feed/social/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 class Profile(models.Model):
     """Create user profiles and manage their attributes"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=180, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to='social/static/social/avatars/', null=True, blank=True) 
